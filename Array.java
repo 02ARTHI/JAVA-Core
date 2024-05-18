44ddcf7948b35b12450bbd446f1167b5fd7d308a
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-18 20:17:53 +0530

    enhanced for loop

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+     public static void main(String[] args) {
+        int num[] = new int[6];
+        num[0]=2;
+        num[1]=3;
+        num[2]=9;
+        num[3]=6;
+        num[4]=5;
+        num[5]=7;
+        
+
+        for(int n : num)
+        {
+            System.out.println(n);
+            
+        }
+
+     }
+}
