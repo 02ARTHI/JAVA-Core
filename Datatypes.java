a6392b7781271fdd302cfc3a66e7954fdd4910c3
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-18 14:21:54 +0530

    Datatype

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+    public static void main(String[] args) {
+    
+    int a =9;
+    float b = 9.9f;
+    double c = 9.9d;
+    char d = 'a';
+    short e=98;
+    long k=345;
+    boolean h = true;
+
+    }
+}
