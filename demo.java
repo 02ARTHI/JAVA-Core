a5d1c1afcb660c4ca6158e0308c32e54dfd20b79
Arthi Selvaraj <164236172+02ARTHI@users.noreply.github.com>
2024-09-07 23:56:09 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,17 +1,16 @@
- class Calculator {
-      
-    public int add(int a, int b)
-    {
-       // System.out.println(a+b);
-        return a+b;
-    }
-}
-
 public class demo
 {
-   public static void main(String[] args) {
-    Calculator c = new Calculator();
-    
-    System.out.println(c.add(10,20));
-   }
+
+    public static void main(String[] args) {
+        double a=20.00;
+        double b=80.00;
+        double c=(a+b)*100.00;
+        double d =c%40.00;
+        Boolean f = (d==0.00)?true:false;
+        System.out.println(f);
+        if(!f)
+        {
+            System.out.println("got some remainder");
+        }
+    }
 }
